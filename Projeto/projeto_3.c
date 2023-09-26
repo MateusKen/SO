d905fef202665a6f7d385dcb90c4054636155dcb
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-09-26 00:40:52 -0300

    Update projeto_3.c

--- Projeto/projeto_3.c
+++ Projeto/projeto_3.c
@@ -12,45 +12,57 @@ sem_t semA, semB,semT;
 void *transfAB(void *args){
   float valorAB = *(float *)args;
   sem_wait(&semT); // Travamento de semaforo Auxiliar para evitar deadlock
-  sem_wait(&semA);
+  sem_wait(&semA); //Travamento recurso saldo A
   printf("\nCliente A travando saldo A\n");
   sleep(2);
-  sem_wait(&semB);
+  sem_wait(&semB); //Travamento recurso saldo B
   printf("Cliente A travando saldo B\n");
+  
   //Lê o próprio saldo
-  FILE *arqA = fopen("saldoA.txt", "w");
+  FILE *arqA = fopen("saldoA.txt", "r");
   if (arqA == NULL){
     printf("Erro ao abrir o arquivo saldoA.txt");
   }
 
   
   fscanf(arqA, "%f", &saldoA);
+  fclose(arqA);
   float saldo_novoA = saldoA - valorAB;  //desconta o valor do saldo de A
-  fprintf(arqA, "%.2f", saldo_novoA);  //atualiza o saldo de A
+  
+  //Atualiza o saldo de A
+  arqA = fopen("saldoA.txt", "w");
+  if(arqA == NULL){
+    printf("Erro ao abrir o arquivo saldoA.txt");
+  }
+  fprintf(arqA, "%.2f", saldo_novoA);
   fclose(arqA);
 
-  printf("Valor de transferencia debitado do saldo A\n");
+  printf("Valor de transferencia (R$%.2f) debitado do saldo A\nNovo saldo A: R$%.2f\n",valorAB,saldo_novoA);
   
   //Transfere para o cliente B
-  FILE *arqB = fopen("saldoB.txt", "w");
+  FILE *arqB = fopen("saldoB.txt", "r");
   if (arqB == NULL){
     printf("Erro ao abrir o arquivo saldoB.txt");
   }
-
-  
   fscanf(arqB, "%f", &saldoB);
+  fclose(arqB);
   float saldo_novoB = saldoB + valorAB;  //saldo de B + valor que A mandou
 
+  //Atualiza o saldo de B
+  arqB = fopen("saldoB.txt", "w");
+  if (arqB == NULL){
+    printf("Erro ao abrir o arquivo saldoB.txt");
+  }
   fprintf(arqB, "%.2f", saldo_novoB); //atualiza saldo de B em saldoB.txt
   fclose(arqB);
 
-  printf("Valor de transferencia creditado ao saldo B\n");
+  printf("Valor de transferencia (R$%.2f) creditado ao saldo B\nNovo saldo B: R$%.2f\n",valorAB,saldo_novoB);
   
-  sem_post(&semB);
+  sem_post(&semB); //Destrava recurso saldo B
   printf("Cliente A destravando saldo B\n");
-  sem_post(&semA);
+  sem_post(&semA); //Destrava recurso saldo A
   printf("Cliente A destravando saldo A\n\n");
-  sem_post(&semT);
+  sem_post(&semT); //Destrava semaforo auxiliar
   pthread_exit(NULL);
   sleep(2);
   return NULL;
@@ -60,41 +72,55 @@ void *transfAB(void *args){
 void *transfBA(void *args){
   float valorBA = *(float *)args;
   sem_wait(&semT); // Travamento de semaforo Auxiliar para evitar deadlock
-  sem_wait(&semB);
+  sem_wait(&semB); //Travamento recurso saldo B
   printf("\nCliente B travando saldo B\n");
   sleep(2);
-  sem_wait(&semA);
+  sem_wait(&semA); //Travamento recurso saldo A
   printf("Cliente B travando saldo A\n");
-  //Lê o próprio saldo
-  FILE *arqB = fopen("saldoB.txt", "r+");
+  
+  //Lê o próprio saldo de B
+  FILE *arqB = fopen("saldoB.txt", "r");
   if (arqB == NULL){
     printf("Erro ao abrir o arquivo saldoB.txt");
   }
-
   fscanf(arqB, "%f", &saldoB);
+  fclose(arqB);
   float saldo_novoB = saldoB - valorBA;  //desconta o valor do saldo de B
-  fprintf(arqB, "%.2f", saldo_novoB);  // atualiza o saldo de B
+
+  //Atualiza o saldo de B
+  arqB = fopen("saldoB.txt", "w");
+  if (arqB == NULL){
+    printf("Erro ao abrir o arquivo saldoB.txt");
+  }
+  fprintf(arqB, "%.2f", saldo_novoB);
   fclose(arqB);
 
-  printf("Valor de transferencia debitado do saldo B\n");
-  //Transfere para o cliente A
-  FILE *arqA = fopen("saldoA.txt", "w");
+  printf("Valor de transferencia (R$%.2f) debitado do saldo B\nNovo saldo B: R$%.2f\n",valorBA,saldo_novoB);
+  
+  //Transfere para o cliente A - pega o saldo de A
+  FILE *arqA = fopen("saldoA.txt", "r");
   if (arqA == NULL){
     printf("Erro ao abrir o arquivo saldoA.txt");
   }
-  
   fscanf(arqA, "%f", &saldoA);
+  fclose(arqA);
   float saldo_novoA = saldoA + valorBA;  //saldo de A + valor que B mandou
-  fprintf(arqA, "%.2f", saldo_novoA); //atualiza saldo de A em saldoA.txt
+
+  //Atualiza o saldo de A
+  arqA = fopen("saldoA.txt", "w");
+  if (arqA == NULL){
+    printf("Erro ao abrir o arquivo saldoA.txt");
+  }
+  fprintf(arqA, "%.2f", saldo_novoA);
   fclose(arqA);
 
-  printf("Valor de transferencia creditado ao saldo A\n");
+  printf("Valor de transferencia (R$%.2f) creditado ao saldo A\nNovo saldo A: R$%.2f\n",valorBA,saldo_novoA);
   
-  sem_post(&semA);
+  sem_post(&semA); //Destrava recurso saldo A
   printf("Cliente B destravando saldo A\n");
-  sem_post(&semB);
+  sem_post(&semB); //Destrava recurso saldo B
   printf("Cliente B destravando saldo B\n\n");
-  sem_post(&semT);
+  sem_post(&semT); // Destrava semaforo auxiliar
   pthread_exit(NULL);
   sleep(2);
   return NULL;
@@ -109,8 +135,8 @@ int main(void) {
   sem_init(&semT, 0, 1);
   
   //declarando valores de transferencias
-  float valorAB = 50;  //50 reais de A para B
-  float valorBA = 100; //200 reais de B para A
+  float valorAB = 300;  //valor em reais de A para B
+  float valorBA = 100; //valor em reais de B para A
   
   //ponteiros para os arquivos de saldoA.txt e saldoB.txt
   FILE *arquivoA;
@@ -133,6 +159,10 @@ int main(void) {
   //fechando os arquivos
   fclose(arquivoA);
   fclose(arquivoB);
+
+  // Print Saldo das contas
+  printf("Saldo A:R$ %.2f\n",saldoA);
+  printf("Saldo B:R$ %.2f\n",saldoB);
   
   //criando threads
   pthread_create(&clienteA, NULL, transfAB, &valorAB);
@@ -142,6 +172,5 @@ int main(void) {
   pthread_join(clienteB, NULL);
 
   sleep(5);
-
   return 0;
 }
